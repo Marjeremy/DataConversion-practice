81745216b1852fe16673ab56394058b750fa877e
Marjeremy <gaoat856@gmail.com>
2024-09-15 10:48:00 +0800

    DataConversion.js commit

---
+++ DataConversion.js
@@ -0,0 +1,32 @@
+// convert string to a number
+let price = '130';
+
+// Using parseInt()
+let priceParsed = parseInt(price);
+console.log('Using parseInt():', priceParsed);
+
+// Using number()
+let priceNumber = Number(price);
+console.log('Using Number:', priceNumber);
+
+// Using + operator 
+let pricePlus = +price;
+console.log('Using + operator:', pricePlus);
+
+// convert a number to string
+let distance = 100;
+
+// Using .toString()
+let distanceToString = distance.toString();
+console.log('Using .toString():', distanceToString);
+
+// Using string()
+let distanceString = string(distance);
+console.log('Using string():', distanceString)
+
+// convert a string to a Decimal Number
+let weight = '20.10';
+
+// Using parseFloat()
+let weightDecimal = parseFloat(weight);
+console.log('Using parseFloat():', weightDecimal);
